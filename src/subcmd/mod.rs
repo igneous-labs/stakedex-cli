4c878d21ac754f0a179e95ed946579b48052c1b1
Johnnycus <sirjsx@gmail.com>
2023-07-07 14:59:23 +0600

    add withdraw-fees cli cmd

--- src/subcmd/mod.rs
+++ src/subcmd/mod.rs
@@ -3,16 +3,19 @@ use clap::Subcommand;
 mod create_fee_acc;
 mod fund_sol_bridge;
 mod list_fee_accs;
+mod withdraw_fees;
 
 pub use create_fee_acc::*;
 pub use fund_sol_bridge::*;
 pub use list_fee_accs::*;
+pub use withdraw_fees::*;
 
 #[derive(Debug, Subcommand)]
 pub enum Subcmd {
     CreateFeeAcc(CreateFeeAccArgs),
     FundSolBridge(FundSolBridgeArgs),
     ListFeeAccs(ListFeeAccsArgs),
+    WithdrawFees(WithdrawFeesArgs),
 }
 
 pub trait SubcmdExec {
@@ -25,6 +28,7 @@ impl SubcmdExec for Subcmd {
             Self::CreateFeeAcc(a) => a.process_cmd(args),
             Self::FundSolBridge(a) => a.process_cmd(args),
             Self::ListFeeAccs(a) => a.process_cmd(args),
+            Self::WithdrawFees(a) => a.process_cmd(args),
         }
     }
 }
