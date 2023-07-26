fa4541db91144048e8940e78f9d1ef45f80cbdc2
billythedummy <dhy1996@live.com.sg>
2023-07-26 20:52:22 +0800

    update slots and reference tx

--- src/subcmd/index/consts.rs
+++ src/subcmd/index/consts.rs
@@ -7,17 +7,17 @@ pub const EALIEST_SIGNATURE: &str =
     "3xFLbx6aBqeAm8z8ZY8gQbx5PFX8gVU1FJbUVvy7Eo4q6bxCyxRTBcG7FU4mRi7qiTQ8KXpCZvQWaozADvmKTmNm";
 
 /// Slot where payer was removed
-pub const PAYER_REMOVED_SLOT: u64 = 206_388_868;
+pub const PAYER_REMOVED_SLOT: u64 = 205_067_324;
 
 /// Transaction in slot PAYER_REMOVED_SLOT where program was updated
 pub const PAYER_REMOVED_SIGNATURE: &str =
-    "4csG1SzE9WZepZdCv1dXfQaD4SAQ7mZXHjBJizP2hZLLkqnVprpq3tPAKrqTBaYEFbJJFx2oSEXTmSwrsjvyLbYX";
+    "tZL1zdBk5P8Q7V9m3qpZ8w8B5SgkSomAjSiEPm1tRKQJvNvaJPoPCougPF5JegAoEEnnZjMiuLzTryUUmSagxmG";
 
-pub const FIRST_NON_ADMIN_SLOT_SINCE_PAYER_REMOVED: u64 = 206_389_107;
+pub const FIRST_NON_ADMIN_SLOT_SINCE_PAYER_REMOVED: u64 = 205_076_752;
 */
 
 pub const FIRST_NON_ADMIN_SIGNATURE_SINCE_PAYER_REMOVED: &str =
-    "2scJfbaU4VbPDiiLqPB9NGT9U6LWXcxNRW2zw2qRNoKUR2UqmeiYJZ18VzmohBMxtrVwyyd6rGPi6VCrRN2SpFrs";
+    "3w9f8YnD8G4ktry66qEYJFYmdSGiNviqdJ5CMv35hAhzXHE9Ub1pzWwTFvidnZ9bWgdPBWEgHfhM3ecmSGEwNASP";
 
 /// RPC limit on `limit` paramter for getSignaturesForAddress
 pub const MAX_SIGNATURES_FOR_ADDRESS_LIMIT: usize = 1_000;
